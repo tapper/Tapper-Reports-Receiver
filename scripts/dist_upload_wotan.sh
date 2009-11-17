b9ff4f8077cd73e472626f00746c43d39627b9db
Steffen Schwigon <steffen.schwigon@amd.com>
2009-11-17 14:14:52 +0100

    adapt new-school dist_upload scripts

--- scripts/dist_upload_wotan.sh
+++ scripts/dist_upload_wotan.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 
+EXECDIR=$(dirname $0)
 DISTFILES='Artemis*-*.*.tar.gz '
+$EXECDIR/../../Artemis/scripts/artemis_version_increment.pl $EXECDIR/../lib/Artemis/Reports/Receiver.pm
+cd $EXECDIR/..
+
+if [[ -e MANIFEST ]]
+then
+  rm MANIFEST
+fi
+./Build manifest || exit -1
+
+perl Build.PL || exit -1
+./Build dist || exit -1
 
 # -----------------------------------------------------------------
 # It is important to not overwrite existing files.
@@ -15,5 +27,5 @@ rsync -vv --progress --ignore-existing ${DISTFILES} artemis@wotan:/home/artemis/
 
 echo ""
 echo '----- re-index -------------------------------------------------'
-ssh artemis@wotan /home/artemis/perl510/bin/cpansite -vl index /home/artemis/CPANSITE/CPAN/
-
+ssh artemis@wotan /home/artemis/perl510/bin/cpansite -v --site=/home/artemis/CPANSITE/CPAN/ --cpan=ftp://ftp.fu-berlin.de/unix/languages/perl/ index
+ssh artemis@wotan /home/artemis/perl510/bin/cpan Artemis::Reports::Receiver
