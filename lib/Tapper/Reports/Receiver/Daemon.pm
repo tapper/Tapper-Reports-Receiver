ffc6b66346303f45037920ecd952911703eb65d1
Steffen Schwigon <steffen.schwigon@amd.com>
2011-02-08 15:42:28 +0100

    rename - src/Tapper-Reports-Receiver (basedir)

---
+++ lib/Tapper/Reports/Receiver/Daemon.pm
@@ -0,0 +1,53 @@
+package Tapper::Reports::Receiver::Daemon;
+
+use 5.010;
+
+use strict;
+use warnings;
+
+use Tapper::Config;
+use Tapper::Reports::Receiver;
+use Moose;
+
+with 'MooseX::Daemonize';
+
+has server => ( is => 'rw');
+has port   => ( is => 'rw', isa => 'Int', default => sub { Tapper::Config->subconfig->{report_port} } );
+
+after start => sub {
+                    my $self = shift;
+
+                    return unless $self->is_daemon;
+
+                    $self->initialize_server;
+                    $self->server->server_loop;
+                   }
+;
+
+sub initialize_server
+{
+        my ($self) = @_;
+        my $EUID = `id -u`; chomp $EUID;
+        my $EGID = `id -g`; chomp $EGID;
+        Tapper::Reports::Receiver->run(
+                                        port         => $self->port,
+                                        log_level    => 2,
+                                        max_servers  => 100,
+                                        user         => $EUID,
+                                        group        => $EGID,
+                                       );
+}
+
+
+sub run
+{
+        my ($self) = @_;
+        my ($command) = @ARGV;
+        return unless $command && grep /^$command$/, qw(start status restart stop);
+        $self->$command;
+        say $self->status_message;
+}
+
+
+1;
+
---
+++ t/00-tapper-meta.t
@@ -0,0 +1,8 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Tapper::Test;
+
+tapper_suite_meta();
